3f5b336e5efe19f85074f71c3302b825b655190b
Viktor Nymark <vnymark@gmail.com>
2019-04-01 22:36:14 +0200

    Created a working solution to part 1.

---
+++ advent_of_code_2/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace advent_of_code_2
+{
+    class Box
+    {
+        public string Id { get; set; }
+        public List<char> TwoTimes { get; set; }
+        public List<char> ThreeTimes { get; set; }
+
+        public void BoxIdToLists()
+        {
+            List<char> TwoTimes = new List<char>();
+            List<char> ThreeTimes = new List<char>();
+            foreach (char c in this.Id)
+            {
+                int matchedTimes = this.Id.Count(x => x == c);
+        
+                if (matchedTimes == 2)
+                {
+                    TwoTimes.Add(c);
+                }
+                if (matchedTimes == 3)
+                {
+                    ThreeTimes.Add(c);
+                    TwoTimes.Remove(c);
+                }
+            }
+            this.TwoTimes = TwoTimes.Distinct().ToList();
+            this.ThreeTimes = ThreeTimes.Distinct().ToList();
+        }
+    }
+}
--- advent_of_code_2/Program.cs
+++ advent_of_code_2/Program.cs
@@ -12,6 +12,64 @@ namespace advent_of_code_2
             var inputPath = @"../../../input.txt";
             //var inputPath = @"../../../test.txt";
             List<string> inputText = File.ReadLines(inputPath).ToList();
+
+            List<Box> BoxList = new List<Box>();
+            int checksum = 0;
+
+            CreateBoxes();
+            AddCounts();
+            GetCountSum(GetTwoTimesCount(), GetThreeTimesCount());
+            Console.WriteLine(checksum);
+            Console.ReadLine();
+
+            void CreateBoxes()
+            {
+                foreach (string row in inputText)
+                {
+                    Box box = new Box();
+                    box.Id = row;
+                    BoxList.Add(box);
+                }
+            }
+
+            void AddCounts()
+            {
+                foreach(Box box in BoxList)
+                {
+                    box.BoxIdToLists();
+                }
+            }
+
+            int GetTwoTimesCount()
+            {
+                int twoTimesCount = 0;
+                foreach (Box box in BoxList)
+                {
+                    if (box.TwoTimes.Count() != 0)
+                    {
+                        twoTimesCount++;
+                    }
+                }
+                return twoTimesCount;
+            }
+
+            int GetThreeTimesCount()
+            {
+                int threeTimesCount = 0;
+                foreach (Box box in BoxList)
+                {
+                    if (box.ThreeTimes.Count() != 0)
+                    {
+                        threeTimesCount++;
+                    }
+                }
+                return threeTimesCount;
+            }
+
+            void GetCountSum(int twoTimesCount, int threeTimesCount)
+            {
+                checksum = twoTimesCount * threeTimesCount;
+            }
         }
     }
 }
